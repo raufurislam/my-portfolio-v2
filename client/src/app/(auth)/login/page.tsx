f3c98a8a5dfa55659f970f707e20cfd2d4f88945
raufurislam <raufurislam@gmail.com>
2025-10-02 21:42:43 +0600

    style(auth-client): setup shadcn and design lagin and registration form

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/modules/auth/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
