4dee756d1e630d01e09a85e339e507f62262804c
raufurislam <raufurislam@gmail.com>
2025-10-08 22:40:37 +0600

    Random render1

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "react/no-unescaped-entities": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
